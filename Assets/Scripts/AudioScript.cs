9d349ef521100b0515728aca7413a8ac33409d55
Pax-Man2 <proan3072@knights.ucf.edu>
2022-10-11 15:13:31 -0400

    Animation commit

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -10,6 +10,13 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
+Animator anim;
+
+void Start()
+{
+  anim = GetComponent<Animator>();
+}
+
 // Update is called once per frame
 void Update()
 {
@@ -17,26 +24,27 @@ void Update()
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
-         }
+          anim.SetInteger("State", 1);
+        }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
-         }
+          anim.SetInteger("State", 0);
+        }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
-         }
+          anim.SetInteger("State", 2);
+        }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
-         }
+          anim.SetInteger("State", 0);
+        }
 
      if (Input.GetKeyDown(KeyCode.L))
         {
